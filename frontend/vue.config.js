51c0434d3fa570071eb845e6f36b2f66341f683f
Stela Dermit <steladermit@MacBook-Air.local>
2025-08-15 13:33:39 +0200

    message

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/',
+  productionSourceMap: false,
   devServer: {
     proxy: {
       '/api': {
